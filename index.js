db2b245ab216a07c4653eadc86663177c1a88895
Kazakov Stepan <ksrt12group@gmail.com>
2021-10-24 23:11:10 +0300

    init
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

---
+++ index.js
@@ -0,0 +1,42 @@
+function calcMatrix() {
+    let matrix = {};
+    const date = document.querySelector("#bd").value;
+    const [year, month, day] = date.split("-").map(i => Number(i));
+
+    const numArr = num => num.toString().split('').map(i => Number(i));
+    const energy3 = year => sum22(numArr(year).reduce((i, g) => i + g));
+    const sum22 = num => {
+        if (num > 22) {
+            const [one, two] = numArr(num);
+            num = one + two;
+        }
+        return +num;
+    };
+
+    matrix.c1 = sum22(day);
+    matrix.c2 = sum22(month);
+    matrix.c3 = energy3(year);
+    matrix.c4 = sum22(matrix.c1 + matrix.c2 + matrix.c3);
+    matrix.c5 = sum22(matrix.c1 + matrix.c2);
+    matrix.c6 = sum22(matrix.c3 + matrix.c2);
+    matrix.c7 = sum22(matrix.c3 + matrix.c4);
+    matrix.c8 = sum22(matrix.c1 + matrix.c4);
+    matrix.c9 = sum22(matrix.c1 + matrix.c2 + matrix.c3 + matrix.c4);
+    matrix.c10 = sum22(matrix.c5 + matrix.c6 + matrix.c7 + matrix.c8);
+    matrix.c11 = sum22(sum22(matrix.c1 + matrix.c3) + sum22(matrix.c2 + matrix.c4));
+    matrix.c12 = sum22(matrix.c9 + matrix.c2);
+    matrix.c13 = sum22(matrix.c9 + matrix.c1);
+    matrix.c14 = sum22(matrix.c13 + matrix.c12);
+    matrix.c15 = sum22(matrix.c12 + matrix.c2);
+    matrix.c16 = sum22(matrix.c1 + matrix.c13);
+    matrix.c17 = sum22(matrix.c9 + matrix.c4);
+    matrix.c18 = sum22(matrix.c9 + matrix.c3);
+    matrix.c19 = sum22(matrix.c18 + matrix.c17);
+    matrix.c20 = sum22(matrix.c4 + matrix.c17);
+    matrix.c21 = sum22(matrix.c3 + matrix.c18);
+    matrix.c22 = sum22(matrix.c21 + matrix.c20);
+
+    Object.keys(matrix).forEach(i => document.querySelectorAll(`#${i}`).forEach(j => j.innerText = matrix[i]));
+    document.querySelector(".numbers").classList.add("show");
+    document.querySelectorAll("#table > tbody > td").forEach(i => i.classList.add("show"));
+}
